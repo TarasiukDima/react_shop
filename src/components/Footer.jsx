52da05cac47fbea8b4a93abc2646563fa4863e1a
TarasiukDima <dimatarasiuk91@gmail.com>
2021-04-17 17:49:07 +0300

    Come fixed!

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
         <footer className="page-footer teal darken-4">
             <div className="footer-copyright">
                 <div className="container">
-                    © {new Date().getFullYear()} Copyright Text <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+                    © {new Date().getFullYear()} Copyright Text <a className="grey-text text-lighten-4 right" href="https://github.com/TarasiukDima/react_shop" target="_blank" rel="noreferrer">Repo</a>
                 </div>
             </div>
         </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ function Header() {
     return (
         <nav className="teal darken-4 nav">
             <div className="nav-wrapper">
-                <a href="#!" className="brand-logo">React Shop</a>
+                <a href="/" className="brand-logo">React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="#!">Repo</a></li>
+                    <li><a href="https://github.com/TarasiukDima/react_shop" target="_blank" rel="noreferrer">Repo</a></li>
                 </ul>
             </div>
         </nav>
